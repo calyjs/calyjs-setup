fd808a2d44ab3b8b879f8ca7cc5ad5023d9322ce
rocmaz <ruben.omh@proton.me>
2025-05-08 12:41:35 -0600

    chore: remove dummy functions

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,8 +2,4 @@ function isEmpty(value: unknown): boolean {
 	return typeof value === 'undefined' || value === null;
 }
 
-function isNumber(value: unknown): value is number {
-	return !isEmpty(value) && typeof value === 'number';
-}
-
-export { isEmpty, isNumber };
+export { isEmpty };
