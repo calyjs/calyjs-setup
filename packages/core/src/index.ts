38759b981049be1eaf14c94ef295172007f35e5b
rocmaz <ruben.omh@proton.me>
2025-05-08 12:38:55 -0600

    chore: add mock functions

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,4 +2,8 @@ function isEmpty(value: unknown): boolean {
 	return typeof value === 'undefined' || value === null;
 }
 
-export { isEmpty };
+function isNumber(value: unknown): value is number {
+	return !isEmpty(value) && typeof value === 'number';
+}
+
+export { isEmpty, isNumber };
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,5 +1,12 @@
 function isString(value: unknown): value is string {
 	return typeof value === 'string' && value.length > 0;
 }
+function isEmptyUtils(value: unknown): boolean {
+	return typeof value === 'undefined' || value === null;
+}
 
-export { isString };
+function isNumberUtils(value: unknown): value is number {
+	return !isEmptyUtils(value) && typeof value === 'number';
+}
+
+export { isString, isNumberUtils, isEmptyUtils };
