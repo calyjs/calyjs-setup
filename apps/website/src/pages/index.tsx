20edabda03e214ae7796e6f4e28c603d467f967e
rocmaz <ruben.omh@proton.me>
2025-05-26 12:16:24 -0600

    feat(website): update link text 4

--- apps/website/src/pages/index.tsx
+++ apps/website/src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
 				<p className="hero__subtitle">{siteConfig.tagline}</p>
 				<div className={styles.buttons}>
 					<Link className="button button--secondary button--lg" to="/docs/intro">
-						Docusaurus Tutorial - 10 min ⏱️ in time 3
+						Docusaurus Tutorial - 10 min ⏱️ in time 4
 					</Link>
 				</div>
 			</div>
