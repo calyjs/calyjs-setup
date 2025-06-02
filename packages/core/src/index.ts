749cd2c16a3bd479dd80c380a79efc28bd21e3f0
rocmaz <ruben.omh@proton.me>
2025-06-02 13:27:50 -0600

    feat(core): add iSrOP function

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,4 +2,8 @@ function isEmpty(value: unknown): boolean {
 	return typeof value === 'undefined' || value === null;
 }
 
-export { isEmpty };
+function isRop(value: unknown): value is number {
+	return typeof value !== 'undefined' && value !== null && Number.isInteger(value);
+}
+
+export { isEmpty, isRop };
