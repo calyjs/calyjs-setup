268b1491eeb785541134159c1505f0c719751a38
rocmaz <ruben.omh@proton.me>
2025-06-02 10:48:03 -0600

    feat(utils): add isStringOther function

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,5 +1,8 @@
 function isString(value: unknown): value is string {
 	return typeof value === 'string' && value.length > 0;
 }
+function isStringOther(value: unknown): value is string {
+	return typeof value === 'string' && value.length > 0;
+}
 
-export { isString };
+export { isString, isStringOther };
