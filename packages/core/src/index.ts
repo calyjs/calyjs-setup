cdd2f7536dcc0e7532e57670a4ea42489fa8a0b7
rocmaz <ruben.omh@proton.me>
2025-06-02 14:25:33 -0600

    feat(core): remove iSrOP function

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,8 +2,4 @@ function isEmpty(value: unknown): boolean {
 	return typeof value === 'undefined' || value === null;
 }
 
-function isRop(value: unknown): value is number {
-	return typeof value !== 'undefined' && value !== null && Number.isInteger(value);
-}
-
-export { isEmpty, isRop };
+export { isEmpty };
