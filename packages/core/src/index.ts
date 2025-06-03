68977c076451c01ccebadfba322b5420ece25f69
rocmaz <ruben.omh@proton.me>
2025-06-03 17:02:13 -0600

    feat(core): remove getString fn

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -5,11 +5,4 @@ function isEmpty(value: unknown): boolean {
 	return typeof value === 'undefined' || value === null;
 }
 
-function getString(): string {
-	if (__DEV__) {
-		console.warn('----- Development mode ------');
-	}
-	return 'string';
-}
-
-export { isEmpty, getString };
+export { isEmpty };
